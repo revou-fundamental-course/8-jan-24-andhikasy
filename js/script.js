9b984c83c022610cf42c887d3a3efd8484bbc7d7
Andhika Saifuddin Yahya <dhikadotcom@gmail.com>
2024-01-18 19:39:00 +0700

    mini-project-SE

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function hitungluas(){
+    square=document.getElementById("square-area").value;
+    luas=square*square;
+    document.getElementById("luas").value=luas;
+    // document.write("L = S x S");
+    // document.write("L = ") + square + ("x") + square;
+    // document.write(luas);
+    }
+function explained(){
+    square=document.getElementById("square-area").value;
+    
+}    
+function reset(){    
+    document.getElementById("squareSide-form").reset();
+}
